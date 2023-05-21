f10dd81dcb8ecd863c2a5348c4eb1d3264a8938b
hiepnguyen90 <johnhiepnguyen1@gmail.com>
2023-05-21 19:05:08 -0400

    done

---
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -0,0 +1,65 @@
+package io.zipcoder.crudapp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @Autowired
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+        
+    }
+
+    @PostMapping("/people")
+    public ResponseEntity<Person> createPerson(@RequestBody Person p){
+        personService.save(p);
+        return new ResponseEntity<>(p, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/people")
+    public ResponseEntity<List<Person>> getPersonList(){
+        personService.findAll();
+        return new ResponseEntity<>(personService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPerson(@PathVariable("id") int id){
+        personService.findById(id);
+        return new ResponseEntity<>(personService.findById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable int id, @RequestBody Person p){
+        Person existingPerson = personService.findById(id);
+
+        p.setId(id);
+        Person savedPerson = personService.save(p);
+
+        if(existingPerson == null){
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedPerson);
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(savedPerson);
+        }
+
+    }
+
+    @DeleteMapping("/people/{id}")
+    public ResponseEntity<?> DeletePerson(@PathVariable int id){
+    personService.deleteById(id);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+}
+}
---
+++ src/main/java/io/zipcoder/crudapp/PersonService.java
@@ -0,0 +1,31 @@
+package io.zipcoder.crudapp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+    @Autowired
+    private PersonRepository personRepository;
+
+    public List<Person> findAll() {
+        return (List<Person>) personRepository.findAll();
+    }
+
+    public Person save(Person person) {
+        return personRepository.save(person);
+    }
+    public Person findById(Integer id) {
+        return personRepository.findOne(id);
+    }
+    
+    public void deleteById(Integer id) {
+        personRepository.delete(id);
+    }
+
+    public Person update(Person person) {
+        return personRepository.save(person);
+    }
+}
